c3968c6aae361e7bde2452808f3c3188c0a6ce5c
Anthony Hortin <anthony@maddisondesigns.com>
2013-06-02 17:02:11 +1000

    Removed clear fix. Added wp_link_pages(). Added wp_kses() & esc_html__(). Added author Bio
    Removed use of clearfix class as containers will now automatically clear
    Added wp_link_pages()
    Added extra use of wp_kses() to ensure only authorised HTML is allowed in translations
    Updated __() references to esc_html__() to ensure any html added into translation file is neutralised
    Added display of author bio

--- content-video.php
+++ content-video.php
@@ -14,13 +14,22 @@
 		<?php }
 		else { ?>
 			<h1 class="entry-title">
-				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'quark' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'quark' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 			</h1>
 		<?php } // is_single() ?>
 		<?php quark_posted_on(); ?>
 	</header> <!-- /.entry-header -->
-	<div class="entry-content clearfix">
-		<?php the_content(); ?>
+	<div class="entry-content">
+		<?php the_content( wp_kses( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'quark' ), array( 
+			'span' => array( 
+				'class' => array() )
+			) ) ); ?>
+		<?php wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'quark' ),
+			'after' => '</div>',
+			'link_before' => '<span class="page-numbers">',
+			'link_after' => '</span>'
+		) ); ?>
 	</div> <!-- /.entry-content -->
 
 	<footer class="entry-meta">
@@ -28,6 +37,10 @@
 			// Only show the tags on the Single Post page
 			quark_entry_meta();
 		} ?>
-		<?php edit_post_link( __( 'Edit', 'quark' ) . ' <i class="icon-angle-right"></i>', '<div class="edit-link">', '</div>' ); ?>
+		<?php edit_post_link( esc_html__( 'Edit', 'quark' ) . ' <i class="icon-angle-right"></i>', '<div class="edit-link">', '</div>' ); ?>
+		<?php if ( is_singular() && get_the_author_meta( 'description' ) && is_multi_author() ) {
+			// If a user has filled out their description and this is a multi-author blog, show their bio
+			get_template_part( 'author-bio' );
+		} ?>
 	</footer> <!-- /.entry-meta -->
 </article> <!-- /#post -->
