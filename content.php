ddd12ac74a9e18a6ef9d4fd2e0edac59c0600d10
Anthony Hortin <anthony@maddisondesigns.com>
2013-06-02 17:52:59 +1000

    Updated __()/_e() references to esc_html__()/esc_html_e() and added extra use of wp_kses(). Call author bio template
    Updated _e() references to esc_html_e() to ensure any html added into translation file is neutralised
    Updated __() references to esc_html__() to ensure any html added into translation file is neutralised
    Added extra use of wp_kses() to ensure only authorised HTML is allowed in translations
    Removed code to display author bio and call template instead

--- content.php
+++ content.php
@@ -10,7 +10,7 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<?php if ( is_sticky() && is_home() && ! is_paged() ) { ?>
 			<div class="featured-post">
-				<?php _e( 'Featured post', 'quark' ); ?>
+				<?php esc_html_e( 'Featured post', 'quark' ); ?>
 			</div>
 		<?php } ?>
 		<header class="entry-header">
@@ -19,12 +19,12 @@
 			<?php }
 			else { ?>
 				<h1 class="entry-title">
-					<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'quark' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+					<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'quark' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 				</h1>
 			<?php } // is_single() ?>
 			<?php quark_posted_on(); ?>
 			<?php if ( has_post_thumbnail() && !is_search() ) { ?>
-				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'quark' ), the_title_attribute( 'echo=0' ) ) ); ?>">
+				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'quark' ), the_title_attribute( 'echo=0' ) ) ); ?>">
 					<?php the_post_thumbnail( 'post_feature_full_width' ); ?>
 				</a>
 			<?php } ?>
@@ -36,10 +36,12 @@
 			</div> <!-- /.entry-summary -->
 		<?php }
 		else { ?>
-			<div class="entry-content clearfix">
-				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'quark' ) ); ?>
+			<div class="entry-content">
+				<?php the_content( wp_kses( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'quark' ), array( 'span' => array( 
+					'class' => array() ) ) )
+					); ?>
 				<?php wp_link_pages( array(
-					'before' => '<div class="page-links">' . __( 'Pages:', 'quark' ),
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'quark' ),
 					'after' => '</div>',
 					'link_before' => '<span class="page-numbers">',
 					'link_after' => '</span>'
@@ -52,36 +54,10 @@
 				// Only show the tags on the Single Post page
 				quark_entry_meta();
 			} ?>
-			<?php edit_post_link( __( 'Edit', 'quark' ) . ' <i class="icon-angle-right"></i>', '<div class="edit-link">', '</div>' ); ?>
-			<?php if ( is_singular() && get_the_author_meta( 'description' ) && is_multi_author() ) { // If a user has filled out their description and this is a multi-author blog, show a bio on their entries. ?>
-				<div class="author-info clearfix">
-					<div class="author-avatar">
-						<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'quark_author_bio_avatar_size', 68 ) ); ?>
-					</div> <!-- /#author-avatar -->
-					<div class="author-description">
-						<h2><?php printf( __( 'About %s', 'quark' ), get_the_author() ); ?></h2>
-						<p><?php the_author_meta( 'description' ); ?></p>
-						<p class="social-meta">
-							<?php if ( get_the_author_meta( 'url' ) ) { ?>
-								<a href="<?php the_author_meta( 'url' ) ?>" title="Website"><i class="icon-link"></i></a>
-							<?php } ?>
-							<?php if ( get_the_author_meta( 'twitter' ) ) { ?>
-								<a href="<?php the_author_meta( 'twitter' ) ?>" title="Twitter"><i class="icon-twitter"></i></a>
-							<?php } ?>
-							<?php if ( get_the_author_meta( 'facebook' ) ) { ?>
-								<a href="<?php the_author_meta( 'facebook' ) ?>" title="Facebook"><i class="icon-facebook-sign"></i></a>
-							<?php } ?>
-							<?php if ( get_the_author_meta( 'googleplus' ) ) { ?>
-								<a href="<?php the_author_meta( 'googleplus' ) ?>" title="Google+"><i class="icon-google-plus"></i></a>
-							<?php } ?>
-						</p>
-						<div class="author-link">
-							<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-								<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'quark' ), get_the_author() ); ?>
-							</a>
-						</div> <!-- /.author-link	-->
-					</div> <!-- /.author-description -->
-				</div> <!-- /.author-info -->
-			<?php } ?>
+			<?php edit_post_link( esc_html__( 'Edit', 'quark' ) . ' <i class="icon-angle-right"></i>', '<div class="edit-link">', '</div>' ); ?>
+			<?php if ( is_singular() && get_the_author_meta( 'description' ) && is_multi_author() ) {
+				// If a user has filled out their description and this is a multi-author blog, show their bio
+				get_template_part( 'author-bio' );
+			} ?>
 		</footer> <!-- /.entry-meta -->
 	</article> <!-- /#post -->
