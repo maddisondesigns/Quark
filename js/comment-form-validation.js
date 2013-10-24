f2fdafe0897332512fed3a4a6e01ee4d0808fcfc
Anthony Hortin <anthony@maddisondesigns.com>
2013-10-24 16:57:20 +1100

    Added check for 'Comment author must fill out name and e-mail' setting when validating comments

--- js/comment-form-validation.js
+++ js/comment-form-validation.js
@@ -2,20 +2,36 @@
  * Handles the client side Comments form validation.
  */
 jQuery( document ).ready( function( $ ) {
-	$( "#commentform" ).validate( {
-		rules: {
-			author: "required",
-			email: {
-				required: true,
-				email: true
+	if( comments_object.req ) {
+
+		$( "#commentform" ).validate( {
+			rules: {
+				author: "required",
+				email: {
+					required: true,
+					email: true
+				},
+				comment: "required"
 			},
-			comment: "required"
-		},
-		messages: {
-			author: '<i class="icon-remove"></i> ' + comments_object.author,
-			email: '<i class="icon-remove"></i> ' + comments_object.email,
-			comment: '<i class="icon-remove"></i> ' + comments_object.comment
-		}
-	} );
+			messages: {
+				author: '<i class="icon-remove"></i> ' + comments_object.author,
+				email: '<i class="icon-remove"></i> ' + comments_object.email,
+				comment: '<i class="icon-remove"></i> ' + comments_object.comment
+			}
+		} );
+
+	}
+	else {
+
+		$( "#commentform" ).validate( {
+			rules: {
+				comment: "required"
+			},
+			messages: {
+				comment: '<i class="icon-remove"></i> ' + comments_object.comment
+			}
+		} );
+
+	}
 
 } );
