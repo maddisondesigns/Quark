fa0f0afca5348212c3bf8581a9611bf685519cf7
George Notaras <gnot@g-loaded.eu>
2013-11-12 16:55:09 +0200

    Use h2 headers for post titles on archives.

--- content.php
+++ content.php
@@ -18,9 +18,9 @@
 				<h1 class="entry-title"><?php the_title(); ?></h1>
 			<?php }
 			else { ?>
-				<h1 class="entry-title">
+				<h2 class="entry-title">
 					<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'quark' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-				</h1>
+				</h2>
 			<?php } // is_single() ?>
 			<?php quark_posted_on(); ?>
 			<?php if ( has_post_thumbnail() && !is_search() ) { ?>
