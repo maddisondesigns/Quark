019420d62a34e93c7a01ee2294bda554ca64deba
Anthony Hortin <anthony@maddisondesigns.com>
2017-02-15 16:17:06 +1100

    Removed comment form from pages using Full-width template pages

--- page-templates/full-width.php
+++ page-templates/full-width.php
@@ -17,7 +17,6 @@ get_header(); ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 					<?php get_template_part( 'content', 'page' ); ?>
-					<?php comments_template( '', true ); ?>
 				<?php endwhile; // end of the loop. ?>
 
 			<?php endif; // end have_posts() check ?>
